d5ff9bc76d37ed9c652fe1d359ad45222f92c33f
Steve Brown <steve@thetasphere.com>
2022-08-11 12:44:29 -0500

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,67 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from 'c3';
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+const chart = c3.generate({
+  bindto: '#chart',
 
-//
+size:{
+  width: 740,
+  height: 480,
+},
+
+axis: {
+  x: {
+    show: true,
+    type: "category",
+  }
+},
+
+
+  data: {
+
+    keys:{
+      x: "month",
+      value:["Peaches","Bananas","Pears"]
+    },
+
+    colors:{
+      Peaches:"red",
+      Bananas:"purple",
+      Pears:"green",
+    },
+
+    type: 'bar', 
+  
+    names: {
+      data1: 'Data Name Z',
+      data2: 'Data Name B'
+    },
+
+
+    json: [
+      {
+        "month": "Jan",
+        "Peaches": 327,
+        "Bananas": 437,
+        "Pears": 111
+      },
+      {
+        "month": "Feb",
+        "Peaches": 555,
+        "Bananas": 454,
+        "Pears": 222
+      },
+      {
+        "month": "Mar",
+        "Peaches": 242,
+        "Bananas": 864,
+        "Pears": 343
+      },
+      {
+        "month": "Apr",
+        "Peaches": 222,
+        "Bananas": 454,
+        "Pears": 434
+      }
+    ]
+  }
+});
